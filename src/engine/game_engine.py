618d6a4ae4099310b28160ab3d470cdd179c46a0
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-05-03 19:47:33 -0500

    Se crea system rendering y load_config_files

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,6 +1,29 @@
+from src.ecs.systems.s_rendering import system_rendering
+import json
+import pygame
+import esper
 class GameEngine:
     def __init__(self) -> None:
+        self._load_config_files()
+
+        pygame.init()
+        pygame.display.set_caption(self.window_cfg["title"])
+        self.screen = pygame.display.set_mode(
+            (self.window_cfg["size"]["w"], self.window_cfg["size"]["h"]),
+            pygame.SCALED)
+
+        self.clock = pygame.time.Clock()
         self.is_running = False
+        self.framerate = self.window_cfg["framerate"]
+        self.delta_time = 0
+        self.bg_color = pygame.Color(self.window_cfg["bg_color"]["r"],
+                                     self.window_cfg["bg_color"]["g"],
+                                     self.window_cfg["bg_color"]["b"])
+        self.ecs_world = esper.World()
+
+    def _load_config_files(self):
+        with open("assets/cfg/window.json", encoding="utf-8") as window_file:
+            self.window_cfg = json.load(window_file)
 
     def run(self) -> None:
         self._create()
@@ -10,6 +33,7 @@ class GameEngine:
             self._process_events()
             self._update()
             self._draw()
+            # await asyncio.sleep(0)
         self._clean()
 
     def _create(self):
@@ -19,13 +43,18 @@ class GameEngine:
         pass
 
     def _process_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
 
     def _update(self):
         pass
 
     def _draw(self):
-        pass
+        self.screen.fill(self.bg_color)
+        system_rendering(self.ecs_world, self.screen)
+        pygame.display.flip()
 
     def _clean(self):
-        pass
+        self.ecs_world.clear_database()
+        pygame.quit()
