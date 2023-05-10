ad1e45f12b282b4f62728a04aa0c2f8cf3785186
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-05-09 20:09:38 -0500

    Se ajusta enemy spawner

--- src/ecs/systems/s_enemy_spawner.py
+++ src/ecs/systems/s_enemy_spawner.py
@@ -4,22 +4,37 @@ import pygame
 
 def system_enemy_spawner(world:esper.World, enemies_info:dict, level_info:list):
    
+    row=0
     for enemy_row in level_info:
+        columns=enemy_row["columns"]
 
-
+        if columns >10:
+            columns=10
+        elif columns<2:
+            columns=2
+        elif columns%2!=0:
+            columns+=1
         
-        
-        for  i in range(enemy_row["columns"]):
-            if enemy_row["enemy_type"]=="Enemy_01":
-                pos_adjust=2+(20*i)
-            elif enemy_row["enemy_type"]=="Enemy_03":
-                pos_adjust=1+(20*i)
-            elif enemy_row["enemy_type"]=="Enemy_04" and i >0:
-                pos_adjust=40+(20*i)
-            else:
-               pos_adjust=20*i 
+        if columns>=4:
+            pos_x=30+10*(10-columns)       
+        else: 
+            pos_x = 90  
 
-            pos = pygame.Vector2(enemy_row["position"]["x"]+pos_adjust,enemy_row["position"]["y"])
+        pos_y=15+15*row
+        for  i in range(columns):
+            pos_adjust=20*i 
+            if columns==2 and i>0:
+                pos_adjust+=40
+
+            if enemy_row["enemy_type"]=="Enemy_03":
+                pos_adjust+=1
+            elif enemy_row["enemy_type"]=="Enemy_01" :
+                pos_adjust+=2          
+
+            pos = pygame.Vector2(pos_x+pos_adjust,pos_y)
 
             create_enemy(world, pos, enemies_info,enemy_row["enemy_type"])
+
+        
+        row+=1
         
